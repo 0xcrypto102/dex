145427fb736464bdca961f50642375d0f4ec3f6b
0xcrypto102 <tiensucha5@gmail.com>
2024-05-08 19:01:46 +0000

    add error

--- programs/dex/src/error.rs
+++ programs/dex/src/error.rs
@@ -1,7 +1,19 @@
 use anchor_lang::prelude::*;
 
 #[error_code]
-pub enum ErrorCode {
-    #[msg("Custom error message")]
-    CustomError,
+pub enum DexError {
+    #[msg("Invalid fee value")]
+    InvalidFee,
+
+    #[msg("Invalid mint for the pool")]
+    InvalidMint,
+
+    #[msg("Depositing too little liquidity")]
+    DepositTooSmall,
+
+    #[msg("Output is below the minimum expected")]
+    OutputTooSmall,
+
+    #[msg("Invariant does not hold")]
+    InvariantViolated,
 }
