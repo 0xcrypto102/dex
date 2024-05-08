1e754b6c1abe1ddf88cfc895a298d9176c6e7836
0xcrypto102 <tiensucha5@gmail.com>
2024-05-08 19:07:17 +0000

    add instructions

--- programs/dex/src/instructions/mod.rs
+++ programs/dex/src/instructions/mod.rs
@@ -1,3 +1,11 @@
-pub mod initialize;
+mod create_amm;
+mod create_pool;
+mod deposit_liquidity;
+mod swap_exact_tokens_for_tokens;
+mod withdraw_liquidity;
 
-pub use initialize::*;
+pub use create_amm::*;
+pub use create_pool::*;
+pub use deposit_liquidity::*;
+pub use swap_exact_tokens_for_tokens::*;
+pub use withdraw_liquidity::*;
