dbdbb0baa7e90c2bf0b8c9bb9b20c2760f67c7dc
0xcrypto102 <tiensucha5@gmail.com>
2024-05-08 19:03:00 +0000

    add fn

--- programs/dex/src/lib.rs
+++ programs/dex/src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(clippy::result_large_err)]
 pub mod constants;
 pub mod error;
 pub mod instructions;
@@ -15,7 +16,32 @@ declare_id!("E5zTLJG8FdWtNZFwHHzwVsN2WKS2ngJtubyusxKgxegE");
 pub mod dex {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn create_amm(ctx: Context<CreateAmm>, id: Pubkey, fee: u16) -> Result<()> {
+        instructions::create_amm(ctx, id, fee)
+    }
+
+    pub fn create_pool(ctx: Context<CreatePool>) -> Result<()> {
+        instructions::create_pool(ctx)
+    }
+
+    pub fn deposit_liquidity(
+        ctx: Context<DepositLiquidity>,
+        amount_a: u64,
+        amount_b: u64,
+    ) -> Result<()> {
+        instructions::deposit_liquidity(ctx, amount_a, amount_b)
+    }
+
+    pub fn withdraw_liquidity(ctx: Context<WithdrawLiquidity>, amount: u64) -> Result<()> {
+        instructions::withdraw_liquidity(ctx, amount)
+    }
+
+    pub fn swap_exact_tokens_for_tokens(
+        ctx: Context<SwapExactTokensForTokens>,
+        swap_a: bool,
+        input_amount: u64,
+        min_output_amount: u64,
+    ) -> Result<()> {
+        instructions::swap_exact_tokens_for_tokens(ctx, swap_a, input_amount, min_output_amount)
     }
 }
